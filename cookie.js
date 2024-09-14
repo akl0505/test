4f8606d9a6d2afee5d1135905c5fdd3910e51f97
mwapiwiki <anhkhoale05052010@outlook.com.vn>
2024-09-14 15:17:14 +0700

    first commit

---
+++ cookie.js
@@ -0,0 +1,42 @@
+// Hàm để lưu cookie
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000)); // thời gian hết hạn cookie
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"; // tạo cookie
+}
+
+// Hàm để đọc cookie
+function getCookie(cname) {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie); // lấy cookie đã mã hóa
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length); // trả về giá trị cookie
+        }
+    }
+    return "";
+}
+
+// Hàm để lưu tên người dùng vào cookie
+function saveUsername() {
+    const username = document.getElementById("username").value;
+    if (username != "") {
+        setCookie("username", username, 7); // lưu tên vào cookie trong 7 ngày
+        alert("Đã lưu tên của bạn: " + username);
+    }
+}
+
+// Hàm để kiểm tra cookie khi trang được tải
+window.onload = function () {
+    const savedUsername = getCookie("username");
+    if (savedUsername != "") {
+        document.getElementById("username").value = savedUsername; // điền giá trị đã lưu vào ô input
+        alert("Xin chào, " + savedUsername + "! Cookie của bạn đã được lưu.");
+    }
+};
